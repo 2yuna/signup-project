f1f577411516e9326a8033940b2bc563250f007a
2yuna <y.duck10002@gmail.com>
2023-03-07 17:02:56 +0900

    first commit

---
+++ src/test/java/yuna/me/signupproject/SignupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package yuna.me.signupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
